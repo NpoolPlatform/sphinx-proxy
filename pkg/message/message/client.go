85633b71b5818ddb3e36deb83b8b50d466801b1e
linuxpizi <1360447275@qq.com>
2021-11-23 15:53:16 +0800

    fix mq vhost

--- pkg/message/message/client.go
+++ pkg/message/message/client.go
@@ -3,12 +3,15 @@ package message
 import (
 	"golang.org/x/xerrors"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/config"
 	msgcli "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/client"
-	constant "github.com/NpoolPlatform/sphinx-proxy/pkg/message/const"
+	ssconst "github.com/NpoolPlatform/sphinx-service/pkg/message/const"
 	msgproducer "github.com/NpoolPlatform/sphinx-service/pkg/message/message"
 
 	"github.com/streadway/amqp"
 )
+
+var vhost = config.ServiceNameToNamespace(ssconst.ServiceName)
 
 type client struct {
 	*msgcli.Client
@@ -18,7 +21,7 @@ type client struct {
 var myClients = map[string]*client{}
 
 func Init() error {
-	_myClient, err := msgcli.New(constant.ServiceName)
+	_myClient, err := msgcli.New(vhost)
 	if err != nil {
 		return err
 	}
@@ -28,7 +31,7 @@ func Init() error {
 		return err
 	}
 
-	sampleClient := &client{
+	client := &client{
 		Client:    _myClient,
 		consumers: map[string]<-chan amqp.Delivery{},
 	}
@@ -38,14 +41,14 @@ func Init() error {
 		return xerrors.Errorf("fail to construct %s consume: %v", msgproducer.GetQueueName(), err)
 	}
 
-	sampleClient.consumers[queue] = deliveries
-	myClients[constant.ServiceName] = sampleClient
+	client.consumers[queue] = deliveries
+	myClients[vhost] = client
 
 	return nil
 }
 
 func ConsumeTrans() (<-chan amqp.Delivery, error) {
-	consumers, ok := myClients[constant.ServiceName].consumers[msgproducer.GetQueueName()]
+	consumers, ok := myClients[vhost].consumers[msgproducer.GetQueueName()]
 	if !ok {
 		return nil, xerrors.Errorf("consumer is not constructed")
 	}
