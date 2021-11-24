3fa2d5fcac40da9aa862b6d81c414d0c4e4d92ba
linuxpizi <1360447275@qq.com>
2021-11-24 11:38:50 +0800

    check conn valid

--- api/manager.go
+++ api/manager.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/rand"
 	"sync"
@@ -19,6 +20,11 @@ import (
 	sconst "github.com/NpoolPlatform/sphinx-service/pkg/message/const"
 	msgproducer "github.com/NpoolPlatform/sphinx-service/pkg/message/message"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
+)
+
+var (
+	ErrNoSignServiceFound   = errors.New("no sign service found")
+	ErrNoPluginServiceFound = errors.New("no plugin service found")
 )
 
 type lmPluginType map[sphinxplugin.CoinType][]*mPlugin
@@ -38,20 +44,29 @@ var (
 	combineProxyChannl = make(chan combineProxy, 1024)
 )
 
-func getProxySign() *mSign {
+func getProxySign() (*mSign, error) {
 	slk.RLock()
 	defer slk.RUnlock()
-	return lmSign[rnd.Intn(len(lmSign))]
+	logger.Sugar().Infof("get proxy sign length: %v", len(lmSign))
+	if len(lmSign) == 0 {
+		return nil, ErrNoSignServiceFound
+	}
+	return lmSign[rnd.Intn(len(lmSign))], nil
 }
 
-func getProxyPlugin(coinType sphinxplugin.CoinType) *mPlugin {
+func getProxyPlugin(coinType sphinxplugin.CoinType) (*mPlugin, error) {
 	plk.RLock()
 	defer plk.RUnlock()
-	return lmPlugin[coinType][rnd.Intn(len(lmPlugin[coinType]))]
+	logger.Sugar().Infof("get proxy plugin length: %v", len(lmPlugin[coinType]))
+	if len(lmPlugin[coinType]) == 0 {
+		return nil, ErrNoPluginServiceFound
+	}
+	return lmPlugin[coinType][rnd.Intn(len(lmPlugin[coinType]))], nil
 }
 
 type mSign struct {
 	signServer signproxy.SignProxy_ProxySignServer
+	close      chan struct{}
 	walletNew  chan *signproxy.ProxySignRequest // address
 	sign       chan *signproxy.ProxySignRequest
 	ack        chan *trading.ACKRequest
@@ -60,6 +75,7 @@ type mSign struct {
 func newSignStream(stream signproxy.SignProxy_ProxySignServer) {
 	lc := &mSign{
 		signServer: stream,
+		close:      make(chan struct{}),
 		walletNew:  make(chan *signproxy.ProxySignRequest),
 		sign:       make(chan *signproxy.ProxySignRequest),
 		ack:        make(chan *trading.ACKRequest),
@@ -165,7 +181,11 @@ func (s *mSign) signStreamRecv() {
 				Address:             ssResponse.GetInfo().GetAddress(),
 			}
 		case signproxy.TransactionType_Signature:
-			pluginProxy := getProxyPlugin(ssResponse.GetCoinType())
+			pluginProxy, err := getProxyPlugin(ssResponse.GetCoinType())
+			if err != nil {
+				logger.Sugar().Error("proxy->plugin no invalid connection")
+				continue
+			}
 			pluginProxy.mpoolPush <- &signproxy.ProxyPluginRequest{
 				CoinType:            ssResponse.GetCoinType(),
 				TransactionType:     ssResponse.GetTransactionType(),
@@ -179,6 +199,7 @@ func (s *mSign) signStreamRecv() {
 
 type mPlugin struct {
 	pluginServer signproxy.SignProxy_ProxyPluginServer
+	close        chan struct{}
 	coinType     sphinxplugin.CoinType              // coin type
 	balance      chan *signproxy.ProxyPluginRequest // address
 	preSign      chan *signproxy.ProxyPluginRequest
@@ -190,6 +211,7 @@ type mPlugin struct {
 func newPluginStream(stream signproxy.SignProxy_ProxyPluginServer) {
 	lp := &mPlugin{
 		pluginServer: stream,
+		close:        make(chan struct{}),
 		balance:      make(chan *signproxy.ProxyPluginRequest),
 		preSign:      make(chan *signproxy.ProxyPluginRequest),
 		mpoolPush:    make(chan *signproxy.ProxyPluginRequest),
@@ -346,7 +368,19 @@ func (p *mPlugin) pluginStreamRecv() {
 			logger.Sugar().Infof("get TransactionIDInsite: %v wallet balance ok", psResponse.GetTransactionIDInsite())
 			continue
 		case signproxy.TransactionType_PreSign:
-			signProxy := getProxySign()
+			signProxy, err := getProxySign()
+			if err != nil {
+				logger.Sugar().Error("proxy->sign no invalid connection")
+				p.ack <- &trading.ACKRequest{
+					IsOkay:              false,
+					TransactionType:     psResponse.GetTransactionType(),
+					TransactionIdInsite: psResponse.GetTransactionIDInsite(),
+					CoinTypeId:          int32(psResponse.GetCoinType()),
+					ErrorMessage:        "proxy->sign no invalid connection",
+				}
+				done <- struct{}{}
+				continue
+			}
 			signProxy.sign <- &signproxy.ProxySignRequest{
 				TransactionType:     psResponse.GetTransactionType(),
 				CoinType:            psResponse.GetCoinType(),
@@ -431,20 +465,32 @@ func ConsumerMQ() error {
 
 		switch tinfo.TransactionType {
 		case signproxy.TransactionType_WalletNew:
-			signStream := getProxySign()
+			signStream, err := getProxySign()
+			if err != nil {
+				logger.Sugar().Error("proxy->sign no invalid connection")
+				goto ackTag
+			}
 			signStream.walletNew <- &signproxy.ProxySignRequest{
 				TransactionType:     tinfo.TransactionType,
 				CoinType:            tinfo.CoinType,
 				TransactionIDInsite: tinfo.TransactionIDInsite,
 			}
 		case signproxy.TransactionType_TransactionNew:
-			mPlugin := getProxyPlugin(tinfo.CoinType)
+			mPlugin, err := getProxyPlugin(tinfo.CoinType)
+			if err != nil {
+				logger.Sugar().Error("proxy->plugin no invalid connection")
+				goto ackTag
+			}
 			combineProxyChannl <- combineProxy{
 				mPlugin: mPlugin,
 				data:    tinfo,
 			}
 		case signproxy.TransactionType_Balance:
-			pluginStream := getProxyPlugin(tinfo.CoinType)
+			pluginStream, err := getProxyPlugin(tinfo.CoinType)
+			if err != nil {
+				logger.Sugar().Error("proxy->plugin no invalid connection")
+				goto ackTag
+			}
 			pluginStream.balance <- &signproxy.ProxyPluginRequest{
 				TransactionType:     tinfo.TransactionType,
 				CoinType:            tinfo.CoinType,
@@ -460,6 +506,18 @@ func ConsumerMQ() error {
 			tinfo.AddressFrom,
 			tinfo.AddressTo,
 		)
+
+	ackTag:
+		{
+			if err := ack(&trading.ACKRequest{}); err != nil {
+				logger.Sugar().Infof(
+					"deal consumer info TranID: %v CoinType: %v error: %v",
+					tinfo.TransactionIDInsite,
+					tinfo.CoinType,
+					err,
+				)
+			}
+		}
 	}
 
 	return nil
