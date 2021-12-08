29bf8b67e1266c8c7e6ae409fdcac020572ab84a
linuxpizi <1360447275@qq.com>
2021-12-08 16:18:16 +0800

    update get transaction

--- api/get_transaction.go
+++ api/get_transaction.go
@@ -6,6 +6,7 @@ import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	"github.com/NpoolPlatform/message/npool/sphinxproxy"
 	"github.com/NpoolPlatform/sphinx-proxy/pkg/crud"
+	"github.com/NpoolPlatform/sphinx-proxy/pkg/db/ent"
 	sconst "github.com/NpoolPlatform/sphinx-proxy/pkg/message/const"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -21,6 +22,10 @@ func (s *Server) GetTransaction(ctx context.Context, in *sphinxproxy.GetTransact
 	defer cancel()
 
 	transInfo, err := crud.GetTransaction(ctx, in.GetTransactionID())
+	if ent.IsNotFound(err) {
+		logger.Sugar().Errorf("GetTransaction TransactionID: %v not found")
+		return &sphinxproxy.GetTransactionResponse{}, status.Errorf(codes.NotFound, "TransactionID: %v not found", in.GetTransactionID())
+	}
 	if err != nil {
 		logger.Sugar().Errorf("GetTransaction call GetTransaction error: %v", err)
 		return &sphinxproxy.GetTransactionResponse{}, status.Error(codes.Internal, "internal server error")
