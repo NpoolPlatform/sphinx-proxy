3b3239592285b95aaa172722dbb4f8b24dc8b58c
linuxpizi <1360447275@qq.com>
2022-07-20 18:28:32 +0800

    resolve confict and support health endpoint

--- api/api.go
+++ api/api.go
@@ -6,6 +6,8 @@ import (
 	"github.com/NpoolPlatform/message/npool/sphinxproxy"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 // Server ..
@@ -15,6 +17,7 @@ type Server struct {
 
 func Register(server grpc.ServiceRegistrar) {
 	sphinxproxy.RegisterSphinxProxyServer(server, &Server{})
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
--- cmd/sphinx-proxy/run.go
+++ cmd/sphinx-proxy/run.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	apimgrcli "github.com/NpoolPlatform/api-manager/pkg/client"
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
@@ -16,14 +18,22 @@ var runCmd = &cli.Command{
 	Name:    "run",
 	Aliases: []string{"r"},
 	Usage:   "Run Sphinx Proxy daemon",
-	After: func(c *cli.Context) error {
+	After: func(*cli.Context) error {
 		if err := grpc2.HShutdown(); err != nil {
 			logger.Sugar().Warnf("graceful shutdown http server error: %v", err)
 		}
 		grpc2.GShutdown()
 		return logger.Sync()
 	},
-	Action: func(c *cli.Context) error {
+	Action: func(*cli.Context) error {
+		podStopSig := make(chan os.Signal, 1)
+		exitChan := make(chan struct{})
+		signal.Notify(podStopSig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		go func() {
+			<-podStopSig
+			logger.Sugar().Info("received SIGTERM to clean conn resource")
+			close(exitChan)
+		}()
 		go func() {
 			if err := grpc2.RunGRPC(rpcRegister); err != nil {
 				logger.Sugar().Warnf("start grpc server error: %v", err)
@@ -34,7 +44,6 @@ var runCmd = &cli.Command{
 	},
 }
 
-// nolint
 func rpcRegister(server grpc.ServiceRegistrar) error {
 	api.Register(server)
 	return nil
