a32ff804ac0d0f3dae9a8283ff4d1163e5d440af
Zhao KK <kikakkz@hotmail.com>
2022-08-03 04:08:03 +0000

    Support create wallet

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -71,3 +71,19 @@ func GetTransaction(ctx context.Context, id string) (*npool.TransactionInfo, err
 	}
 	return info.(*npool.TransactionInfo), nil
 }
+
+func CreateAddress(ctx context.Context, coinName string) (*npool.WalletInfo, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.SphinxProxyClient) (cruder.Any, error) {
+		resp, err := cli.CreateWallet(ctx, &npool.CreateWalletRequest{
+			Name: coinName,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail create wallet: %v", err)
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail create wallet: %v", err)
+	}
+	return info.(*npool.WalletInfo), nil
+}
